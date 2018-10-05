337614fff78ef00b93e4acc5330ed718b151726d
Thompson Plyler <nealok.g@gmail.com>
2018-10-05 12:30:02 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [1, 2, 3, 4]
+  array
 end
 
 def add_element_to_end_of_array(array, element)
-  [1,2,3,4].push(element)
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
